764c1d1f892cc89110f601ff3122ed8e24ab7806
Julio <julio@ctrlsun.com>
2021-09-11 22:00:58 -0500

    Added some comments.

--- base/views.py
+++ base/views.py
@@ -9,6 +9,7 @@ from .serializers import ProductSerializer
 
 @api_view(["GET"])
 def list_products(request):
+    """Listing products"""
     products = Product.objects.all().order_by("-createdAt")
     serializer = ProductSerializer(products, many=True)
 
